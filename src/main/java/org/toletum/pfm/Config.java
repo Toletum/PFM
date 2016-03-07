ada8bed3f5287c72921a7a2da611386e179f71b6
Carlos Sanchez <toletum@toletum.org>
2016-03-07 09:23:21 +0100

    Prueba Streaming JDBC

--- src/main/java/org/toletum/pfm/Config.java
+++ src/main/java/org/toletum/pfm/Config.java
@@ -4,6 +4,11 @@ public interface Config {
 	public static final String KafkaServer = "kafka:9092";
 	public static final String KafkaTopicCrime = "crimes";
 	public static final String KafkaTopicCommand = "command";
+	public static final String KafkaTopicClock = "clock";
 
 	public static final String ZooKeeperServer = "zookeeper:2181";
+	
+	public static final String RedisServer = "database";
+	public static final String RedisCrimes = "Crimes";
+	public static final String RedisClock = "Clock";
 }
